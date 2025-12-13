2aea7e8aad61ee54fe53e9b5acc905a23f994b4f
Muhammad Mukhtar <yusoofjnr@gmail.com>
2025-12-13 13:54:10 +0100

    chore: configure axios client with base URL and auth interceptor

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api/v1",
+  baseURL: "https://raw.githubusercontent.com/V4vamp/lendsqr-test/main",
   headers: {
     "Content-Type": "application/json",
   },
--- src/types/types.ts
+++ src/types/types.ts
@@ -26,16 +26,59 @@ export interface UserResponse {
   email: string;
 }
 
-export interface DashboardStats {
-  sales_value: number;
-  commission_earned: number;
-  currency: string;
+
+export interface Guarantor {
+  fullName: string;
+  phone: string;
+  email: string;
+  relationship: string;
 }
 
-export interface Transaction {
-  player_name: string;
-  date: string;
-  amount: number;
-  currency: string;
-  status: string;
+export interface Education {
+  level: string;
+  employmentStatus: string;
+  sector: string;
+  duration: string;
+  officeEmail: string;
+  monthlyIncome: string;
+  loanRepayment: number;
+}
+
+export interface Account {
+  balance: string;
+  accountNumber: number;
+  bankName: string;
+}
+
+export interface Profile {
+  fullName: string;
+  avatar: string;
+  phone: string;
+  bvn: number;
+  gender: string;
+  maritalStatus: string;
+  children: string;
+  residence: string;
+}
+
+export interface Socials {
+  twitter: string;
+  facebook: string;
+  instagram: string;
+}
+
+export interface User {
+  id: string;
+  organization: string;
+  username: string;
+  email: string;
+  phone?: string;
+  dateJoined: string;
+  status: 'Active' | 'Inactive' | 'Pending' | 'Blacklisted';
+  tier: number;
+  account: Account;
+  profile: Profile;
+  education: Education;
+  socials: Socials;
+  guarantors: Guarantor[];
 }
