8e123874670ad2c8ef8a25f9c5238fe027d0bc16
Muhammad Mukhtar <yusoofjnr@gmail.com>
2025-12-18 09:43:24 +0100

    feat: final ui correction, ready for submission

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,7 +10,7 @@ return (
     <div className={styles.notFound}>
         <h1>404 - Page Not Found</h1>
         <p>The page you are looking for does not exist.</p>
-        <button onClick={() => router.push("/dashboard/users")}>Go to Home</button>
+        <button onClick={() => router.push("/dashboard/users")}>Go to Dashboard</button>
       </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+"use client";
 
 import styles from "./page.module.scss";
-import SideBar from "@/components/SideBar/SideBar";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <SideBar />
-      </main>
+      <h1>Welcome</h1>
+      <p>to</p>
+      <Image src={"/images/logo.png"} alt="lendsqr" width={144} height={30} />
+      <button onClick={() => router.push("/dashboard/users")}>
+        Go to Dashboard
+      </button>
     </div>
   );
 }
